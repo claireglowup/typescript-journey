37fcad9dbbed687e659f79835fa15e4a51a9633f
claire <claireglowup@gmail.com>
2025-01-24 05:55:07 +0700

    generics

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,3 @@
-class SomeClass {
-  constructor(public value: string) {}
-}
-
-type SomeConstructor = {
-  new (s: string): SomeClass;
-};
-
-function createInstance(ctor: SomeConstructor, arg: string) {
-  return new ctor(arg);
-}
-
-// Contoh penggunaan
-const instance = createInstance(SomeClass, "hello");
-console.log(instance.value); // Output: "hello"
-
-function tests(): void {
-  return "nb";
+function identity<Type>(arg: Type): Type {
+  return arg;
 }
